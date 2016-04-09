a1bfa07c4993b996453cf94c87974519f7f6d48c
Srujan <srujant@gmail.com>
2016-04-09 05:25:54 -0400

    have some sound

--- app/src/main/java/com/example/jack/tapjam/DrumActivity.java
+++ app/src/main/java/com/example/jack/tapjam/DrumActivity.java
@@ -1,11 +1,8 @@
 package com.example.jack.tapjam;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -13,33 +10,19 @@ import android.widget.ImageButton;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class DrumActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+public class DrumActivity extends Activity {
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-
-    private View mContentView;
-    private View mControlsView;
-    private boolean mVisible;
 
     private ImageButton snare;
     private ImageButton hihat;
     private ImageButton kick;
     private ImageButton clap;
+
+    MediaPlayer snare_sound;
+    MediaPlayer hihat_sound;
+    MediaPlayer kick_sound;
+    MediaPlayer clap_sound;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,133 +30,44 @@ public class DrumActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_drum);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
 
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
         snare = (ImageButton) findViewById(R.id.snare);
         hihat = (ImageButton) findViewById(R.id.hihat);
         kick = (ImageButton) findViewById(R.id.kick);
         clap = (ImageButton) findViewById(R.id.clap);
-    }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+        snare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snare_sound = MediaPlayer.create(getApplicationContext(), R.raw.snare);
+                snare_sound.start();
             }
-            return false;
-        }
-    };
+        });
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
+        hihat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hihat_sound = MediaPlayer.create(getApplicationContext(), R.raw.hihat);
+                hihat_sound.start();
             }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
+        });
 
-    private final Handler mHideHandler = new Handler();
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
+        kick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kick_sound = MediaPlayer.create(getApplicationContext(), R.raw.kick);
+                kick_sound.start();
+            }
+        });
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+        clap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clap_sound = MediaPlayer.create(getApplicationContext(), R.raw.clap);
+                clap_sound.start();
+            }
+        });
     }
+
 }
