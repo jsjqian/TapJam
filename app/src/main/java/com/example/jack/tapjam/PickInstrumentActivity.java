ffa0f998d258a87e3c11231ddf6993a69c5897ea
jsjqian <jack.qian97@gmail.com>
2016-04-09 01:01:38 -0400

    Create basic activities to be programmed

---
+++ app/src/main/java/com/example/jack/tapjam/PickInstrumentActivity.java
@@ -0,0 +1,13 @@
+package com.example.jack.tapjam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickInstrumentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_instrument);
+    }
+}
