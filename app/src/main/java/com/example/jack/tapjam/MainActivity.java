f6742eb7af4329fb011ca8f696a1befa4a2d58ad
jsjqian <jack.qian97@gmail.com>
2016-04-09 09:40:50 -0400

    connected buttons

--- app/src/main/java/com/example/jack/tapjam/MainActivity.java
+++ app/src/main/java/com/example/jack/tapjam/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.jack.tapjam;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = (Button)findViewById(R.id.start_button);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateSessionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/jack/tapjam/PickInstrumentActivity.java
+++ app/src/main/java/com/example/jack/tapjam/PickInstrumentActivity.java
@@ -1,13 +1,36 @@
 package com.example.jack.tapjam;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PickInstrumentActivity extends AppCompatActivity {
+public class PickInstrumentActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_instrument);
+
+        Button drum = (Button)findViewById(R.id.drum);
+        Button synth = (Button)findViewById(R.id.synth);
+
+        drum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PickInstrumentActivity.this, DrumActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        synth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PickInstrumentActivity.this, Synth.class);
+                startActivity(intent);
+            }
+        });
     }
 }
