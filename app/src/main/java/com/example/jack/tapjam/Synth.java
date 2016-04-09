817f3e4063d675fe94d66c0052dc4c170f212558
George <gtong@terpmail.umd.edu>
2016-04-09 11:12:42 -0400

    Merge branch 'master' of https://github.com/jsjqian/TapJam
    # Conflicts:
    #	app/app.iml

--- app/src/main/java/com/example/jack/tapjam/Synth.java
+++ app/src/main/java/com/example/jack/tapjam/Synth.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 
 /**
@@ -22,12 +23,12 @@ import android.widget.ImageButton;
 public class Synth extends Activity {
 
     ImageButton b1, b2, b3, b4, b5, b6, b7, b8;
+    LinearLayout L;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_synth);
-
         b1 = (ImageButton) findViewById(R.id.imageButton1);
         b1.setClickable(false);
         b2 = (ImageButton) findViewById(R.id.imageButton2);
@@ -44,22 +45,49 @@ public class Synth extends Activity {
         b7.setClickable(false);
         b8 = (ImageButton) findViewById(R.id.imageButton8);
         b8.setClickable(false);
+        L = (LinearLayout) findViewById(R.id.layout);
 
-        b1.setOnTouchListener(new View.OnTouchListener() {
+        L.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
 
-                if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
-                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.f1);
-                    mp.start();
-                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        public void onCompletion(MediaPlayer mp) {
-                            mp.release();
-                        }
-                    });
-
+//                if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+//                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.f1);
+//                    mp.start();
+//                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                        public void onCompletion(MediaPlayer mp) {
+//                            mp.release();
+//                        }
+//                    });
+                int width = v.getWidth();
+//                int height = v.getHeight();
+                float x = event.getX();
+//                float y = event.getY();
+                String msg;
+                if (x < width / 8) {
+                    sound1(v);
                 }
-
-                return true;
+                else if(width/8 < x && x < width/4) {
+                    sound2(v);
+                }
+                else if(width/4 < x && x < 3*width/8) {
+                    sound3(v);
+                }
+                else if(3*width/8 < x && x < width/2) {
+                    sound4(v);
+                }
+                else if(width/2 < x && x < 5*width/8) {
+                    sound5(v);
+                }
+                else if(5*width/8 < x && x < 3*width/4) {
+                    sound6(v);
+                }
+                else if(3*width/4 < x && x < 7*width/8) {
+                    sound7(v);
+                }
+                else if(7*width/8 < x) {
+                    sound8(v);
+                }
+                return false;
             }
         });
     }
