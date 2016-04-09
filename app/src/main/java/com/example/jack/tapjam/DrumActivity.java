b708e52c35ab9ffe463b0fbf14af4c79650305d0
Srujan <srujant@gmail.com>
2016-04-09 12:40:28 -0400

    pre george

--- app/src/main/java/com/example/jack/tapjam/DrumActivity.java
+++ app/src/main/java/com/example/jack/tapjam/DrumActivity.java
@@ -3,10 +3,12 @@ package com.example.jack.tapjam;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
-import java.io.IOException;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -19,6 +21,9 @@ public class DrumActivity extends Activity {
     private ImageButton hihat;
     private ImageButton kick;
     private ImageButton clap;
+    private boolean isRunning = false;
+
+    private Button metronome;
 
 
     @Override
@@ -31,62 +36,98 @@ public class DrumActivity extends Activity {
         hihat = (ImageButton) findViewById(R.id.hihat);
         kick = (ImageButton) findViewById(R.id.kick);
         clap = (ImageButton) findViewById(R.id.clap);
+        metronome = (Button) findViewById(R.id.metronome);
 
 
-        snare.setOnClickListener(new View.OnClickListener() {
+        snare.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.snare);
-                mp.start();
-                mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    }
-                });
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.snare);
+                    mp.start();
+                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                        }
+                    });
 
-
+                }
+                return true;
             }
         });
 
-        hihat.setOnClickListener(new View.OnClickListener() {
+        hihat.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.hihat);
-                mp.start();
-                mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    }
-                });
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.hihat);
+                    mp.start();
+                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                        }
+                    });
 
+                }
+                return true;
             }
         });
 
-        kick.setOnClickListener(new View.OnClickListener() {
+        kick.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.kick);
-                mp.start();
-                mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    }
-                });
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.kick);
+                    mp.start();
+                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                        }
+                    });
 
+                }
+                return true;
             }
         });
 
-        clap.setOnClickListener(new View.OnClickListener() {
+        clap.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.clap);
+                    mp.start();
+                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                        }
+                    });
+
+                }
+                return true;
+            }
+        });
+
+        metronome.setOnClickListener(new View.OnClickListener() {
+            MediaPlayer metro;
             @Override
             public void onClick(View v) {
-                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.clap);
-                mp.start();
-                mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    }
-                });
 
+                if (!isRunning) {
+                    metro = MediaPlayer.create(getApplicationContext(), R.raw.metronome);
+                    Log.d("ISRUNNING", "Is running is: " + isRunning);
+                    metro.start();
+                    metro.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                        }
+                    });
+                    isRunning = true;
+
+                }else{
+                    Log.d("ISRUNNING", "Is running is: " + isRunning);
+                    metro.release();
+                    isRunning = false;
+                }
             }
         });
     }
